d9aa715b21829f56539b786a9f45fe6d72cc1216
paulpham157 <phamdinhhung2302@gmail.com>
2025-03-09 16:16:43 +0700

    clear sample

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ujh6qCR5FYJEwREfzviOiVkdt+N9Jpe2Mv3mU9SBIHo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
